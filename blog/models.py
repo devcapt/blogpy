aeb3930e2f0bbbb0e3ac0be5a00b78a1321926e1
devcapt <mohammaddev7@gmail.com>
2021-07-27 12:11:12 +0430

    added custom admin model

--- blog/models.py
+++ blog/models.py
@@ -12,14 +12,11 @@ def validate_file_extension(value):
   if ext.lower() not in valid_extensions:
     raise ValidationError('Unsupported  file extension.')
 
-
 class UserProfile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE) # NOTE:dont re-invent tire use default user things
   avatar = models.FileField(upload_to='files/user_cover',null=True,blank=True,validators=[validate_file_extension])
   description = models.CharField(max_length=512,null=False,blank=False)
 
-  def __str__(self):
-    return f'{self.user.first_name} {self.user.last_name}'
 
 class Article(models.Model):
   title = models.CharField(max_length=128,null=False,blank=False)
@@ -29,11 +26,7 @@ class Article(models.Model):
   category = models.ForeignKey('Category',on_delete=models.CASCADE)
   author = models.OneToOneField(UserProfile,on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.title
+
 class Category(models.Model):
   title = models.CharField(max_length=128,null=False,blank=False)
   cover = models.FileField(upload_to='files/category_cover',null=False,blank=False,validators=[validate_file_extension])
-  
-  def __str__(self):
-    return self.title
