5d3142b571bb7dd2e4d3a1ec4f1af9fb4053d6ee
devcapt <mohammaddev7@gmail.com>
2021-08-08 10:22:28 +0430

    6 api maded,contact page linked,serializers maded

--- blogpy/settings.py
+++ blogpy/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third partty dependencys
     'ckeditor',
+
+    # my apps
     'blog',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- blogpy/urls.py
+++ blogpy/urls.py
@@ -23,8 +23,13 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'^ckeditor/', include('ckeditor_uploader.urls')), # maybe url
-    path('',include('blog.urls'))
+    path('',include('blog.urls')),
+    
+    # DRF
+    path('api-auth/', include('rest_framework.urls'))
+    
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+    urlpatterns += static('contact/static/',document_root=settings.STATIC_ROOT)
